84ed729e52f90ee860f66d19dece356fd30f8871
Nishant Mukhija <nishantmukhija76@gmail.com>
2018-02-14 13:35:17 +0530

    Merge into descending
    Identification: 001
    Change-Id: I90b3e6682e7e4921245f8dd7f908f572db8207d5

---
+++ DataStructureProblems_Practice/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge2ArraysinDesc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 2, 4, 7, 8, 44, 55, 65 };
+            int[] arr2 = { 2, 4, 6 };
+            Merge2ArraysinDesc(arr1, arr2);
+            Console.ReadKey();
+        }
+        private static void Merge2ArraysinDesc(int[] arr1, int[] arr2)
+        {
+            int j = 0;
+            int k = 0;
+            int totalLength = arr1.Length + arr2.Length;
+            int[] resultarr = new int[arr1.Length + arr2.Length];
+            for (int i = resultarr.Length - 1; i >= 0; i--)
+            {
+                if (arr1.Length == j)
+                {
+                    resultarr[i] = arr2[k++];
+                }
+                else if (arr2.Length == k)
+                {
+                    resultarr[i] = arr1[j++];
+                }
+                else if (arr1[j] <= arr2[k])
+                {
+                    resultarr[i] = arr1[j++];
+                }
+                else if (arr1[j] > arr2[k])
+                {
+                    resultarr[i] = arr2[k++];
+                }
+
+            }
+            foreach (var i in resultarr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ DataStructureProblems_Practice/readme.txt
@@ -0,0 +1,10 @@
+﻿Problem states that, there are two arrays given in ascending order and you have to merge them into a new array in descending order.
+
+Input:
+
+Array 1 = 2, 4, 7, 8, 44, 55, 65
+Array 2 = 2, 4, 6
+
+Output:
+
+65, 55, 44, 8, 7, 6, 4, 4, 2, 2
